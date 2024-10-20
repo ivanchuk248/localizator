86c80a5e1c25c3fd6611636ffa7e1f12c7be5fad
ibarajas248 <ibarajas@unal.edu.co>
2024-10-20 13:11:53 -0500

    hackaton1m

--- mapas.py
+++ mapas.py
@@ -11,14 +11,23 @@ with open('ingreso.html', 'r', encoding='utf-8') as f:
 st.components.v1.html(ingreso, height=600)
 
 # Título de la aplicación
-st.title("GANACIA/PERDIDA NETA")
+st.title("SECTORES")
 
 
-with open('ingreso.html', 'r', encoding='utf-8') as f:
-    ingreso = f.read()
+with open('mapa_geolocalizado.html', 'r', encoding='utf-8') as f:
+    mapa_geolocalizado = f.read()
 
 # Mostrar el mapa en la aplicación de Streamlit
-st.components.v1.html(ingreso, height=600)
+st.components.v1.html(mapa_geolocalizado, height=600)
+
+st.title("DIRECTORIO COMPETENCIA")
+
+
+with open('mapa_geolocalizado_buscador.html', 'r', encoding='utf-8') as f:
+    mapa_geolocalizado_buscador = f.read()
+
+# Mostrar el mapa en la aplicación de Streamlit
+st.components.v1.html(mapa_geolocalizado_buscador, height=600)
 
 
 
